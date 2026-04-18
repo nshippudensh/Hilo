6c0f65329f2d08dd83320d435d02b8861ade3ff5
DoritoAzul <nshippudensh@gmail.com>
2026-04-17 20:33:44 -0600

    Cambio de nombres

--- inc/threadpool.h
+++ inc/threadpool.h
@@ -1,5 +1,5 @@
-#ifndef THREADPOOL
-#define THREADPOOL
+#ifndef THREADPOOL_H
+#define THREADPOOL_H
 
 #include <pthread.h>
 
@@ -7,24 +7,25 @@
 #define QUEUE_SIZE 100
 
 typedef struct {
-  void (*fn)(void* arg); // A pointer to the function that will ve executed
-  void* arg;             // A pointer to the argument that will be passed to the function
-}task_t;
-
+  void (*fn)(void* arg);
+  void* arg;
+} task_t;
 
 typedef struct {
-  pthread_t threads[THREADS];  // An array of threads that make up the thread pool
-  pthread_mutex_t lock;           // A mutex used to synchronize access to the task queue
-  pthread_cond_t notify;          // A condition variable to notify threads when a new task is available
-  task_t task_queue[QUEUE_SIZE];  // An array representing the queue of task waiting to be executed
-  int queued;                     // The maximum size of the task queue
-  int queue_front;                // The index of the front of the task queue
-  int queue_back;                 // The index of the rear of the task queue
-  int stop;                       // A flag to indicate when the thread pool should stop processing tasks and terminate
-}threadpool_t;
+  pthread_mutex_t lock;
+  pthread_cond_t notify;
+  pthread_t threads[THREADS];
+  task_t task_queue[QUEUE_SIZE];
+  int queued;
+  int queue_front;
+  int queue_back;
+  int stop;
+} threadpool_t;
 
-void threadpool_init(threadpool_t *);
-void threadpool_destroy(threadpool_t *);
-void threadpool_add_task(threadpool_t *, void (*function)(void*), void* arg);
-void example_task(void *arg);
-#endif
+// Function declarations
+void threadpool_init(threadpool_t* pool);
+void threadpool_destroy(threadpool_t* pool);
+void threadpool_add_task(threadpool_t* pool, void (*function)(void*), void* arg);
+void example_task(void* arg);
+
+#endif // THREADPOOL_H 
